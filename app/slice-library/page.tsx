475ceb374af12aae187a9632db39b15a2bac0a60
Tomek "jaen" Mańko <hi@jaen.me>
2024-05-24 15:40:50 +0200

    Documentation and code clarity improvements

--- app/slice-library/page.tsx
+++ app/slice-library/page.tsx
@@ -6,6 +6,16 @@ import { SharedSliceContent } from "@prismicio/types-internal/lib/content";
 
 import { SliceLibrary } from "./SliceLibrary";
 
+export interface SliceLibrary {
+  name: string;
+  slices: SliceWithMocks[];
+}
+
+interface SliceWithMocks {
+  model: SharedSlice;
+  mocks: Partial<Record<string, SharedSliceContent>>;
+}
+
 export async function generateMetadata(): Promise<Metadata> {
   return {
     title: "Slice Library",
@@ -13,73 +23,79 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-export const dynamic = 'force-static'
+export const dynamic = "force-static";
 
 export default async function SliceLibraryPage() {
+  const libraries = await loadSliceLibraries();
 
-  const libraries = await extractModels();
-
-  return (
-    <SliceLibrary libraries={libraries} />
-  );
+  return <SliceLibrary libraries={libraries} />;
 }
 
-async function extractModels(): Promise<({ slices: SharedSlice[], name: string, mocks: SharedSliceContent[][] }[])> {
-
+// Load all the slice libraries defined for this Prismic project
+async function loadSliceLibraries(): Promise<SliceLibrary[]> {
   try {
-    const file = await fs.readFile(process.cwd() + '/slicemachine.config.json', 'utf8');
+    const file = await fs.readFile(
+      process.cwd() + "/slicemachine.config.json",
+      "utf8"
+    );
     const config = JSON.parse(file);
-    
-    const libraries =
-      await Promise.all(
-        (config.libraries || []).map(async (library: string) => {
-          return {
-            name: library,
-            slices: await readModels<SharedSlice>({
-              path: path.join(process.cwd(), library),
-              fileName: "model.json",
-            }),
-            mocks: await readModels<SharedSliceContent[]>({
-              path: path.join(process.cwd(), library),
-              fileName: "mocks.json",
-            })
-          };
-        }),
-      )
+
+    const libraries = await Promise.all(
+      (config.libraries || []).map(loadSliceLibrary)
+    );
 
     return libraries;
-  }
-  catch {
-    throw new Error("Issue when reading local slice libraries listed in slicemachine.config.json");
+  } catch {
+    throw new Error(
+      "Issue when reading local slice libraries listed in slicemachine.config.json"
+    );
   }
 }
 
-const readModels = async <TType extends SharedSliceContent[] | SharedSlice>(args: {
-  path: string;
-  fileName: string;
-}): Promise<TType[]> => {
-  const entries = await fs.readdir(args.path, {
+// Load all slices and their associated mocks for the given slice library
+async function loadSliceLibrary(library: string): Promise<SliceLibrary> {
+  const libraryPath = path.join(process.cwd(), library);
+  const entries = await fs.readdir(libraryPath, {
     recursive: true,
     withFileTypes: true,
   });
 
-  const results: TType[] = [];
+  const slices: SliceWithMocks[] = [];
 
-  //Order slices alphabetically in each library
-  entries.sort((a,b)=> a.path>b.path ? 1 : -1)
+  // Order slices alphabetically in each library
+  entries.sort((a, b) => (a.path > b.path ? 1 : -1));
 
   for (const entry of entries) {
-    if (entry.name !== args.fileName || entry.isDirectory()) {
+    if (entry.name !== "model.json" || entry.isDirectory()) {
       continue;
     }
 
-    const contents = await fs.readFile(
-      path.join(entry.path, entry.name),
-      "utf8",
-    );
+    const modelPath = path.join(entry.path, entry.name);
+    const mocksPath = path.join(entry.path, "mocks.json");
 
-    results.push(JSON.parse(contents));
+    const modelContents = await fs.readFile(modelPath, "utf8");
+    const model = JSON.parse(modelContents) as SharedSlice;
+    const mocks: SliceWithMocks["mocks"] = {};
+
+    // Read mocks for the given slice – if mocks are not present, ignore that;
+    // the Slice Library components will display an empty placeholder in that case
+    try {
+      const mocksContents = await fs.readFile(mocksPath, "utf-8");
+      const mocksJson = JSON.parse(mocksContents) as SharedSliceContent[];
+
+      // Create a map from variation name to it's mocks for easy access in components
+      for (const { id } of model.variations) {
+        const mock = mocksJson.find(({ variation }) => variation === id);
+
+        mocks[id] = mock;
+      }
+    } catch (e) {}
+
+    slices.push({ model, mocks });
   }
 
-  return results;
-};
+  return {
+    name: library,
+    slices,
+  };
+}
